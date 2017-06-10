81c6bb1dcbbe56f70763ef665d315dbb8e6051a8
Gabriel Cesar <gcal1@live.com>
2017-06-10 16:52:09 -0300

    up

--- Union_Find/union_find.cpp
+++ Union_Find/union_find.cpp
@@ -1,17 +1,34 @@
 #include "union_find.hpp"
 
 // Aloca memória, inicializa e retorna o nó.
-Node* make_set(char id)
-{
-
+Node * make_set(char id) {
+    Node * no = new Node();
+    no->id = id;
+    no->rank = 0;
+    no->pai = no;
+    return no;
 }
 
-Node* find_set(Node *x)
-{
-
+Node * find_set(Node * x) {
+    if (x->pai != x)
+        x->pai = find_set(x->pai);
+    return x->pai;
 }
 
-void uf_union(Node *x, Node *y)
-{
+Node * find_set2(Node * x) {
+    if (x->pai == x)
+        return x->pai;
+    x->pai = find_set2(x->pai);
+}
 
+void uf_union(Node * x, Node * y) {
+    Node * nox = find_set(x);
+    Node * noy = find_set(y);
+    if (nox->rank > noy->rank)
+         noy->pai = nox;
+    else {
+        nox->pai = noy;
+        if (nox->rank == noy->rank)
+            noy->rank++;
+    }
 }
--- Union_Find/union_find.hpp
+++ Union_Find/union_find.hpp
@@ -1,10 +1,10 @@
 
 struct Node {
-   struct Node *pai;
+   struct Node *  pai;
    int rank;
    char id; // identificador
 };
 
-Node* make_set(char id); // Aloca memória, inicializa e retorna o nó.
-Node* find_set(Node *x);
-void uf_union(Node *x, Node *y);
+Node * make_set(char id); // Aloca memória, inicializa e retorna o nó.
+Node * find_set(Node * x);
+void uf_union(Node * x, Node * y);
